4b6eb5a80be6a443fd3ff6faf9e818724e59f07c
Oscar <oscardelafuentetoledo10@gmail.com>
2023-07-14 14:56:23 +0200

    añadido firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCq0OgBqG3IR0XhvikeyAGc3hsDjVj4UeQ",
+    authDomain: "eventos-1d5ff.firebaseapp.com",
+    databaseURL: "https://eventos-1d5ff-default-rtdb.firebaseio.com",
+    projectId: "eventos-1d5ff",
+    storageBucket: "eventos-1d5ff.appspot.com",
+    messagingSenderId: "449387759194",
+    appId: "1:449387759194:web:6e881fec55bffb04c3ba63"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCq0OgBqG3IR0XhvikeyAGc3hsDjVj4UeQ",
+    authDomain: "eventos-1d5ff.firebaseapp.com",
+    databaseURL: "https://eventos-1d5ff-default-rtdb.firebaseio.com",
+    projectId: "eventos-1d5ff",
+    storageBucket: "eventos-1d5ff.appspot.com",
+    messagingSenderId: "449387759194",
+    appId: "1:449387759194:web:6e881fec55bffb04c3ba63"
+  },
 };
 
 /*
