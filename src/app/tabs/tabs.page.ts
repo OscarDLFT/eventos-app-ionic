b877e0ba859addc1f4359efdc437ed42461d40c7
Oscar <oscardelafuentetoledo10@gmail.com>
2023-08-01 14:30:18 +0200

    añadido notificaciones permisos

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PushNotificationSchema, PushNotifications } from '@capacitor/push-notifications';
+import { NotificationsService } from '../service/notifications.service';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
 
-  constructor() {}
+  constructor(
+    private notification: NotificationsService,
+  ) {}
+
+  ngOnInit(): void {
+    const isPushNotificationvailable = Capacitor.isPluginAvailable('PushNotifications');
+    if(!!isPushNotificationvailable){
+      PushNotifications.requestPermissions().then(result => {
+        if(result?.receive){
+          PushNotifications.register();
+        } else {
+          console.error("No tienes permisos");
+        }
+      });
+  
+      PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+        this.notification.addNotification(notification);
+        console.log(JSON.stringify(notification));
+      });
+    }
+  }
 
 }
